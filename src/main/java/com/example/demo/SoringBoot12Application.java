15bb7e5b1bfab3ae5e4d8921ee5031a59d84837b
GBTC440015UR <GBTC440015UR@MC.MONTGOMERYCOLLEGE.EDU>
2018-03-14 11:17:17 -0400

    Initial commit.

---
+++ src/main/java/com/example/demo/SoringBoot12Application.java
@@ -0,0 +1,12 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SoringBoot12Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SoringBoot12Application.class, args);
+	}
+}
